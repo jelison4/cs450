cb6aa022f468926c01b4c2da5f15c193c0693b18
josh <joshelison@gmail.com>
2020-02-18 13:53:37 -0700

    ;lkh'i

--- Neural_Network/neuron.py
+++ Neural_Network/neuron.py
@@ -2,7 +2,7 @@ import random
 
 class Neuron:
     def __init__ (self, value, weight, isBias):
-        #self.weight = random.uniform(-10,10)
+        self.weight = random.uniform(-1.0,1.0)
         self.weight = weight
         self.value = value
         self.isBias = isBias
--- Prove03-Elison.py
+++ Prove03-Elison.py
@@ -44,8 +44,6 @@ def KNNAccuracy(data, target, k):
 #######################################################################
 # Regression junk
 #######################################################################
-
-
 def regressionAccuracy(data, target):
     X_train, X_test, y_train, y_test = train_test_split(
         data, target, test_size=0.2, random_state=24)
